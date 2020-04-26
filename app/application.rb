f75de5d030f8fb7b30742ce3663696a8ffaf1d3c
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-04-26 02:41:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,8 +6,6 @@ def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
 if req.path=="/items"
-    resp.write ""
-  else
     resp.write "Route not found"
     resp.status = 404
 end
