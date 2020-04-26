f9e3bf2c5182cf7c9b4c5a9530f497544ca2d298
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-04-26 02:39:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
 if req.path.match(/items/)
-    item_price = req.path.split("/items/").last
+    item_name = req.path.split("/items/").last
     item = @@items.find{|i| i.name == item_price}
     resp.write item.name
   else
