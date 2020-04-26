f453a5332939233f1c796add8762bf5434ae7266
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-04-26 02:39:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ def call(env)
   req = Rack::Request.new(env)
 if req.path.match(/items/)
     item_name = req.path.split("/items/").last
-    item = @@items.find{|i| i.name == item_price}
+    item = @@items.find{|i| i.name == item_name}
     resp.write item.name
   else
     resp.write "Route not found"
