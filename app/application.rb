7788cfcd087335ac55c397d36fdf2200dcc6897f
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-04-26 02:50:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,17 +1,26 @@
 class Application
 
-@@items = []
+  @@items = []
 
- def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+    def call(env)
+      req = Rack::Request.new(env)
+      resp = Rack::Response.new
 
-    if req.path=="/items"
-      resp.write "Route not found"
-      resp.status = 404
+      if req.path.match(/items\/.+/)
+        item_name = req.path.split("/items/").last
+        item = @@items.detect { |i| i.name == item_name }
+        if item
+          resp.write item.price
+        else
+          resp.write "Item not found"
+          resp.status = 400
+        end
+      else
+        resp.write "Route not found"
+        resp.status = 404
+      end
+
+      resp.finish
     end
-
-    resp.finish
-  end
 
 end
