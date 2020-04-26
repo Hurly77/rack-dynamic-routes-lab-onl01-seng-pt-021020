69f9df709c0db7a6e0c23957fa9189c7dc1832f7
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-04-26 02:56:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
       resp = Rack::Response.new
 
       if req.path.match(/items/)
-
+        item_price = req.path.split("/items/").last
       else
         resp.write "Route not found"
         resp.status = 404
