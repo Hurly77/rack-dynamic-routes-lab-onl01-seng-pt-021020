f2565999644cc3fc85fe48a018cda0b53ded0f2d
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-04-26 02:31:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
 def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
-if req.path=="/items/price"
+if req.path.match(/items/)
     resp.write @@items[0].price
   elsif req.path=="/items/price"
 
