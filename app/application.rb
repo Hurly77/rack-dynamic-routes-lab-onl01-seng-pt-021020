c323af8b7fdd0cdb681456f9a5384fb8baac8c2b
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-04-26 02:41:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,14 +5,12 @@ class Application
 def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
-if req.path.match(/items/)
-    item_name = req.path.split("/items/").last
-    item = @@items.find{|i| i.name == item_name}
-    resp.write item.name
+if req.path=="/items"
+    resp.write ""
   else
     resp.write "Route not found"
     resp.status = 404
-  end
+end
 
 
 resp.finish
